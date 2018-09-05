33ee19d90342566aa0bc522665eceefaaa9549ae
Erik Bergen <ebergen@live.ca>
2018-09-05 15:11:42 -0600

    Simple Addition in Java

---
+++ SimpleAddition.java
@@ -0,0 +1,12 @@
+/**
+ * SimpleAddition
+ */
+public class SimpleAddition {
+
+    public static void main(String[] args) {
+        int x=4;
+        int y=5;
+        int z= x+y;
+        System.out.println(z);
+    }
+}
