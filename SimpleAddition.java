a52295a69f49077489b0ec4fa9cd55d898af34d2
Erik Bergen <ebergen@live.ca>
2018-09-05 15:16:39 -0600

    Changed Int values

--- SimpleAddition.java
+++ SimpleAddition.java
@@ -4,8 +4,8 @@
 public class SimpleAddition {
 
     public static void main(String[] args) {
-        int x=4;
-        int y=5;
+        int x=6;
+        int y=8;
         int z= x+y;
         System.out.println(z);
     }
